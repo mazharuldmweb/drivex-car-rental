255d78224b572c129b4435d08ae347a8fb01eabf
mazharuldmweb <jihadseopro@gmail.com>
2026-05-29 01:19:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                red: { 500: '#e63329', 600: '#c8281f', 700: '#a01f18' },
+                dark: { DEFAULT: '#0a0a0a', 2: '#111113', 3: '#1a1a1e', 4: '#222228' },
+            },
+            fontFamily: {
+                sans: ['Barlow', 'sans-serif'],
+                cond: ['Barlow Condensed', 'sans-serif'],
+            }
+        }
+    }
+}
